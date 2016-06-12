931c9704ce3eb1b6299f4e123b28c8898823f2a2
root <grahamkeenan@hotmail.co.uk>
2016-06-13 00:24:44 +0100

    Still working on the pop script

--- DadPubList/populate.py
+++ DadPubList/populate.py
@@ -5,18 +5,34 @@ import django
 django.setup()
 
 from publist.models import Pub
+import re
 
 def populate():
 	f = open('publist.txt', 'r')
 
-	while f.readline != '':
+	while f.readline() != '':
 		l = f.readline()
-		
-		if l[:-3].endswith('Y'):
-			print l
+		words = l.split(',')
+		last = words[-1]
+		if len(words) == 6:
+			print words
+		"""
+		if 'Y' in last:
+			split = last.split('**')
+			if '**' in last:
+				addPub(words[0], words[1], words[2], split[0], True, True)
+			else:
+				addPub(words[0], words[1], words[2], split[0], True, False)
 		else:
-			print "No Y"
-	
+			if len(words) == 4:
+				addPub(words[0], words[1], words[2], words[3], False, False)
+			elif len(words) == 3:
+				addPub(words[0], words[1], words[2], "None", False, False)
+		"""
+	f.close()
+		
+		
+
 
 def addPub(name, address, area, county, thisyear, fromapp):
 	p = Pub.objects.get_or_create(name=name, address=address, area=area, county=county, thisyear=thisyear, fromapp=fromapp)
