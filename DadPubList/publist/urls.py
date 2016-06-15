b655ee0fe84c566f0db7effc451c840e87763cd7
ShinRa26 <grahamkeenan@hotmail.com>
2016-06-15 21:44:04 +0100

    Working on Adding a Pub functionality

--- DadPubList/publist/urls.py
+++ DadPubList/publist/urls.py
@@ -4,5 +4,6 @@ from publist import views
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),
 	url(r'^results/$', views.search, name='results'),
+	url(r'^addpub/$', views.add_pub, name='addpub'),
 	#url(r'^results/$', views.results, name='results'),
 )
